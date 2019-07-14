f2e64d27433c22eeb9b1e5fc8153e6d2c2f8ef78
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2019-07-14 18:04:10 +0300

    completed abot activity

---
+++ app/src/main/java/ke/co/davidwanjohi/alcphase1/AboutActivity.java
@@ -0,0 +1,34 @@
+package ke.co.davidwanjohi.alcphase1;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.webkit.WebView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView.findViewById(R.id.webview);
+
+        webView.loadUrl("https://andela.com/alc/");
+        setContentView(webView);
+
+
+    }
+
+}
